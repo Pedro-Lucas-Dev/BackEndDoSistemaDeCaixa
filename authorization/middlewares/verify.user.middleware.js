ac8bbb7e2db6bcb327bf20f52d2debf3a9004109
Charley Oliveira <charleycesar@gmail.com>
2021-04-15 21:17:56 -0300

    fix: add message for user has not found

--- authorization/middlewares/verify.user.middleware.js
+++ authorization/middlewares/verify.user.middleware.js
@@ -25,7 +25,7 @@ exports.hasAuthValidFields = (req, res, next) => {
 exports.isPasswordAndUserMatch = (req, res, next) => {
     UserModel.findByEmail(req.body.email).then((user) => {
         if (!user[0]) {
-            res.status(404).send({})
+            return res.status(401).send({errors: ['Users does not exists']})
         } else {
             let passwordFields = user[0].password.split('$');
             let salt = passwordFields[0];
